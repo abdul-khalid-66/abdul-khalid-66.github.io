a368fce5d539ca9bf2fc5413a6e1aff92b2811f0
Abdul Khalid <65578359+abdul-khalid-66@users.noreply.github.com>
2025-02-20 19:13:46 +0500

    Update Script.js

--- js/Script.js
+++ js/Script.js
@@ -1,8 +1,18 @@
 // typing animation 
 
-var typed =new typed(".typing",{
-    strings:["Web Designer","Web Developer","Graphic Designer","Serch Engien Optimization"],
-    typeSpeed:100,
-    BackSpeed:60,
-    loop:true
-})
+// var typed =new typed(".typing",{
+//     strings:["Web Designer","Web Developer","Graphic Designer","Serch Engien Optimization"],
+//     typeSpeed:100,
+//     BackSpeed:60,
+//     loop:true
+// })
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".typing", {
+        strings: ["Web Designing/Development", "HTML/CSS/Bootstrap", "PHP/Laravel", "JavaScript/jQuery/Ajax", "Stripe Integration"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
